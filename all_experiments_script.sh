e6bcc6ba5029aa179733bbb0ae934a9fb75dbf75
evabrinkk <64408531+evabrinkk@users.noreply.github.com>
2021-04-21 14:25:08 +0200

    changed the name of experiments (to run in server)

--- all_experiments_script.sh
+++ all_experiments_script.sh
@@ -8,33 +8,33 @@
 echo 'Experiment 99 TEST finished'
 
 # E0 BaselineAllScenarios
-~/Downloads/NetLogo6.1.1/netlogo-headless.sh --model Model_Coupled_withoutplots_4152021.nlogo --experiment E0_BaselineAllScenarios(10x) --table E0_BaselineAllScenarios(10x).csv --threads 1
+~/Downloads/NetLogo6.1.1/netlogo-headless.sh --model Model_Coupled_withoutplots_4152021.nlogo --experiment E0_BaselineAllScenarios --table E0_BaselineAllScenarios.csv --threads 1
 echo 'Experiment 0 finished'
 
 # E1 AllPoliciesCompetitive0
-~/Downloads/NetLogo6.1.1/netlogo-headless.sh --model Model_Coupled_withoutplots_4152021.nlogo --experiment E1_AllPoliciesCompetitive0(10x) --table E1_AllPoliciesCompetitive0(10x).csv --threads 1
+~/Downloads/NetLogo6.1.1/netlogo-headless.sh --model Model_Coupled_withoutplots_4152021.nlogo --experiment E1_AllPoliciesCompetitive0 --table E1_AllPoliciesCompetitive0.csv --threads 1
 echo 'Experiment 1 finished'
 
 
 # E2
-~/Downloads/NetLogo6.1.1/netlogo-headless.sh --model Model_Coupled_withoutplots_4152021.nlogo --experiment E2_AllPoliciesCompetitive10(10x) --table E2_AllPoliciesCompetitive10(10x).csv --threads 1
+~/Downloads/NetLogo6.1.1/netlogo-headless.sh --model Model_Coupled_withoutplots_4152021.nlogo --experiment E2_AllPoliciesCompetitive10 --table E2_AllPoliciesCompetitive10.csv --threads 1
 echo 'Experiment 2 finished'
 
 # E3
-~/Downloads/NetLogo6.1.1/netlogo-headless.sh --model Model_Coupled_withoutplots_4152021.nlogo --experiment E3_AllPoliciesCompetitive20(10x) --table E3_AllPoliciesCompetitive20(10x).csv --threads 1
+~/Downloads/NetLogo6.1.1/netlogo-headless.sh --model Model_Coupled_withoutplots_4152021.nlogo --experiment E3_AllPoliciesCompetitive20 --table E3_AllPoliciesCompetitive20.csv --threads 1
 echo 'Experiment 3 finished'
 
 
 # E4
-~/Downloads/NetLogo6.1.1/netlogo-headless.sh --model Model_Coupled_withoutplots_4152021.nlogo --experiment E4_AllPoliciesCompetitive30(10x) --table E4_AllPoliciesCompetitive30(10x).csv --threads 1
+~/Downloads/NetLogo6.1.1/netlogo-headless.sh --model Model_Coupled_withoutplots_4152021.nlogo --experiment E4_AllPoliciesCompetitive30 --table E4_AllPoliciesCompetitive30.csv --threads 1
 echo 'Experiment 4 finished'
 
 # E5
-~/Downloads/NetLogo6.1.1/netlogo-headless.sh --model Model_Coupled_withoutplots_4152021.nlogo --experiment E5_AllPoliciesCompetitive40(10x) --table E5_AllPoliciesCompetitive40(10x).csv --threads 1
+~/Downloads/NetLogo6.1.1/netlogo-headless.sh --model Model_Coupled_withoutplots_4152021.nlogo --experiment E5_AllPoliciesCompetitive40 --table E5_AllPoliciesCompetitive40.csv --threads 1
 echo 'Experiment 5 finished'
 
 # E6
-~/Downloads/NetLogo6.1.1/netlogo-headless.sh --model Model_Coupled_withoutplots_4152021.nlogo --experiment E6_BaselineTimeSlotAllScenarios(10x) --table E6_BaselineTimeSlotAllScenarios(10x).csv --threads 1
+~/Downloads/NetLogo6.1.1/netlogo-headless.sh --model Model_Coupled_withoutplots_4152021.nlogo --experiment E6_BaselineTimeSlotAllScenarios --table E6_BaselineTimeSlotAllScenarios.csv --threads 1
 echo 'Experiment 6 finished'
 
 # SA
